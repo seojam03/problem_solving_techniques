4b15fc073e2d2d5498650e74d55a2a461e502f52
seojam03 <131682547+seojam03@users.noreply.github.com>
2025-09-15 23:32:55 +0900

    Update pyramid.c

--- 2주차/pyramid.c
+++ 2주차/pyramid.c
@@ -1,64 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-typedef struct{
+typedef struct {
     int value;
     int index;
 } S;
 
-int compare(const void *a, const void *b){
+int compare(const void* a, const void* b) {
     int num1 = ((S*)a)->value;
     int num2 = ((S*)b)->value;
     return num2 - num1;
 }
 
-int smaller(int a, int b, int c){
-    if(a<b && a<c){
+int smaller(int a, int b, int c) {
+    if (a < b && a < c) {
         return a;
-    } else if(a<b){
+    }
+    else if (a < b) {
         return c;
-    } else if(b>c){
+    }
+    else if (b > c) {
         return c;
-    } else {
+    }
+    else {
         return b;
     }
 }
 
-int recursive_pyramid(int num, int *lss, int index, int max){
-    if(num == 0 || max>=(num*2+1)) return num;
-    for(int i=1 ; i<=num ; i++){
-        if((lss[index - i] < num - i + 1) || (lss[index + i] < num - i + 1) || (lss[index - num + i - 1] < i) || (lss[index + num - i + 1] < i)){
-            return recursive_pyramid(num - 1, lss, index, max);
+int recursive_pyramid(int num, int* lss, int index, int max) {
+    int check = 0;
+    if (num == 0) return 1;
+    for (int i = max / 2 + 1; i <= num; i++) {
+        for (int j = 0; j < i; j++) {
+            if ((lss[index - i + j] < j + 1) || (lss[index + i - j] < j + 1)) {
+                return max  + check;
+            }
         }
+        check += 2;
     }
-    return num;
+    return check + max;
 }
 
-int main(void){
-    int num, max_len=0, limit, input_it;
-    scanf("%d",&num);
-    int *origin_lss = (int *)malloc(sizeof(int)*num);
-    S *lss = (S *)malloc(sizeof(S)*num);
-    for(int i=0 ; i<num ; i++){
-        scanf("%d",&lss[i].value);
+int main(void) {
+    int num, max_len = 1, limit, input_it;
+    scanf("%d", &num);
+    int* origin_lss = (int*)malloc(sizeof(int) * num);
+    S* lss = (S*)malloc(sizeof(S) * num);
+    for (int i = 0; i < num; i++) {
+        scanf("%d", &lss[i].value);
         origin_lss[i] = lss[i].value;
         lss[i].index = i;
     }
-    qsort(lss,num,sizeof(S),compare);
-    
-    for(int i=0 ; max_len < lss[i].value * 2 - 1 ; i++){
-        limit = smaller(lss[i].index*2+1, (num - lss[i].index)*2-1, lss[i].value*2-1);
-        if(limit <= max_len) continue;
-        
-        input_it = recursive_pyramid(limit/2, origin_lss, lss[i].index, max_len)*2 + 1;
-        if(input_it > max_len){
+    clock_t start, end;
+    start = clock();
+    qsort(lss, num, sizeof(S), compare);
+
+    for (int i = 0; max_len < lss[i].value * 2 - 1; i++) {
+        limit = smaller(lss[i].index * 2 + 1, (num - lss[i].index) * 2 - 1, lss[i].value * 2 - 1);
+        if (limit <= max_len) continue;
+
+        input_it = recursive_pyramid(limit / 2, origin_lss, lss[i].index, max_len);
+        if (input_it > max_len) {
             max_len = input_it;
         }
     }
-    printf("%d",max_len);
+    printf("%d\n", max_len);
 
     free(lss);
     free(origin_lss);
+
+    end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Execution time: %lf seconds\n", time_taken);
+
     return 0;
 }
